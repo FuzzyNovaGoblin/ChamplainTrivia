3dc040b3027ce32d070b88982cc79cc9659fc154
Redempt <instantiable@gmail.com>
2022-10-01 16:01:49 -0400

    Add persistent scoreboard data

---
+++ app/src/main/java/com/fuzytech/champlaintrivia/highscore/Scoreboard.kt
@@ -0,0 +1,36 @@
+package com.fuzytech.champlaintrivia.highscore
+
+import java.io.File
+
+object Scoreboard {
+
+    private lateinit var file: File
+
+    fun init(file: File) {
+        this.file = file
+    }
+
+    fun file(quiz: String) = file.resolve("${quiz}.txt")
+
+    fun scores(quiz: String): MutableMap<String, Int> {
+        val map = HashMap<String, Int>()
+        file(quiz).readLines().forEach {it.split(" ").also {map[it[0]] = it[1].toInt()}}
+        return map
+    }
+
+    fun highScores(quiz: String, limit: Int): List<String> {
+        val scores = scores(quiz)
+        val players = ArrayList<String>()
+        players.addAll(scores.keys)
+        players.sortByDescending { scores[it] }
+        return players.map { "$it ${scores[it]}" }.take(limit)
+    }
+
+    fun addScore(quiz: String, player: String, score: Int) {
+        val scores = scores(quiz)
+        scores[player] = score
+        val contents = scores.entries.map {(player, score) -> "$player $score"}.joinToString("\n")
+        file(quiz).writeText(contents)
+    }
+
+}
